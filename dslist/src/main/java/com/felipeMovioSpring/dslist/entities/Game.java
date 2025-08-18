908ed07eaf5574dcd6e9102ee4938e0edd657ab9
FelipeMovio <felipelsmovio@gmail.com>
2025-08-18 13:26:20 -0300

    maisAvanços
    properties, Game, ORM

---
+++ dslist/src/main/java/com/felipeMovioSpring/dslist/entities/Game.java
@@ -0,0 +1,131 @@
+package com.felipeMovioSpring.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+
+@Entity // configurar minha classe java para que ela seja equivalente a uma tabela do banco relacional
+@Table(name = "tb_game")
+public class Game{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // id
+    private String title; // titulo
+
+    @Column(name = "game_year")
+    private Integer year; // ano
+    private String genre; // genero
+    private String platform; // plaataforma
+    private Double score; // pontuação
+    private String imgUrl;
+    private String shortDescription; // pequena descrição
+    private String longDescription; // longa descrição
+
+
+    public Game(){
+
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platform, Double score,
+                String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platform = platform;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+
+
+
+    // metodos para usar para comparar dois games
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
